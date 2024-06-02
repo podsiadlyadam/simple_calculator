a88d2e25237dc4f5939594fc953c4df9a0b9232a
adampodsiadly <podsiadloadam198@gmail.com>
2024-06-02 07:56:14 -0700

    Add calculator component

---
+++ src/components/calculator/Calculator.js
@@ -0,0 +1,15 @@
+export const Calculator = () => {
+  return (
+    <div
+      style={{
+        width: "460px",
+        height: "640px",
+        backgroundColor: "#272829",
+        borderRadius: "24px",
+        borderWidth: "2px",
+        borderColor: "#ffffff",
+        borderStyle: "solid",
+      }}
+    ></div>
+  );
+};
